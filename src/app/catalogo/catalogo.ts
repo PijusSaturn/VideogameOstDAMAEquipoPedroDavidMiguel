3cf4c1cce24e3d06bc5bd9c36c862835e68092fb
PijusSaturn <pedrocolladomartinez41@gmail.com>
2026-05-11 22:18:48 +0200

    Añadido catalogo json

--- src/app/catalogo/catalogo.ts
+++ src/app/catalogo/catalogo.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-catalogo',
   imports: [],
-  templateUrl: './catalogo.html',
-  styleUrl: './catalogo.css',
+  templateUrl: '/catalogo.component.html',
+  styleUrl: '/catalogo.css',
 })
 export class Catalogo {}
